c98745367accefbe3feee41811e71f4b5d73bfef
oznakdn <ozanakaydin@gmail.com>
2024-02-28 12:52:26 +0300

    first commit

---
+++ Creational/BuilderPattern/Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Text;
+
+
+
+CarBuilder builder = new CarBuilder();
+
+Car mustang = Car.Create(builder);
+ builder.SetBrand("Ford")
+ .SetModel("Mustang")
+ .SetMaxSpeed(320)
+ .SetColor(Color.White)
+ .WriteToConsole();
+
+ Car focus = Car.Create(builder);
+ builder.SetBrand("Ford")
+ .SetModel("Focus")
+ .SetMaxSpeed(230)
+ .SetColor(Color.Gray)
+ .WriteToConsole();
+
+/*Output
+
+ID: a381651d-2f41-4cd7-87ad-0a0437a37d95
+Brand: Ford
+Model: Mustang
+Color: White
+Speed: 320
+
+ID: 4bb86b31-9634-49db-8e7b-9e98176f94a9
+Brand: Ford
+Model: Focus
+Color: Gray
+Speed: 230
+
+*/
+
+
+class Car
+{
+    public string Id {get;private set;}
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string Color { get; set; }
+    public int MaxSpeed { get; set; }
+    
+    private Car(){}
+
+    public static Car Create(CarBuilder carBuilder)
+    {
+        carBuilder.Car = new Car();
+        carBuilder.Car.Id = Guid.NewGuid().ToString();
+        return carBuilder.Car;
+    }
+
+}
+
+
+class CarBuilder
+{
+    public Car Car {get;set;}
+    public CarBuilder SetBrand(string brand)
+    {
+        Car.Brand = brand;
+        return this;
+    }
+
+    public CarBuilder SetModel(string model)
+    {
+        Car.Model = model;
+        return this;
+    }
+
+    public CarBuilder SetMaxSpeed(int speed)
+    {
+        Car.MaxSpeed = speed;
+        return this;
+    }
+
+    public CarBuilder SetColor(Color color)
+    {
+        Car.Color = color.ToString();
+        return this;
+    }
+
+    public CarBuilder WriteToConsole()
+    {
+        var result = new StringBuilder();
+        result.AppendLine($"ID: {Car.Id}");
+
+        if(!string.IsNullOrEmpty(Car.Brand))
+        {
+            result.Append($"Brand: {Car.Brand}\n");
+        }
+
+        if(!string.IsNullOrEmpty(Car.Model))
+        {
+            result.Append($"Model: {Car.Model}\n");
+        }
+
+        if(!string.IsNullOrEmpty(Car.Color))
+        {
+            result.Append($"Color: {Car.Color}\n");
+        }
+
+         if(Car.MaxSpeed > 0)
+        {
+            result.Append($"Speed: {Car.MaxSpeed}\n");
+        }
+
+        Console.WriteLine(result.ToString());
+        return this;
+    }
+
+}
+
+enum Color
+{
+    Black,
+    White,
+    Gray,
+    Blue,
+    Red,
+    Green
+}
+
+
+
+
+
+
---
+++ Creational/SingletonPattern/Program.cs
@@ -0,0 +1,102 @@
+﻿
+/*
+The purpose of this Singleton class is to ensure only one instance of a class T can be created. It allows controlling access to a single shared instance across the application.
+
+The Singleton class takes a generic type parameter T which specifies the type of object to make singleton. T must satisfy the constraint that it can be created using the 'new()' constructor.
+
+The Instance method takes an instance of T as input and returns an instance of T as output. It first checks if the static _instance field is null, indicating no instance has been created yet. If so, it creates a new T using 'new()', assigns it to _instance, and also assigns the passed input instance to _instance. This overrides the newly created instance with the provided one. Finally, _instance is returned.
+
+So the first time Instance is called, it will create a new T instance and return it. On subsequent calls, it will keep returning the same _instance field, ensuring only one instance of T exists. The input instance is only used the first time to initialize the singleton. After that, any passed instances are ignored.
+
+This allows creating a singleton class where the first instance can be provided from the caller, while still ensuring future callers receive the same instance. The singleton instance is initialized lazily on first use.
+
+*/
+
+
+// Usage
+var firstInstance = Singleton<Connection>.Instance(new Connection { ConnectionString = "firstConnection" });
+var secodInstance = Singleton<Connection>.Instance(new Connection { ConnectionString = "secondConnection" });
+Console.Write(firstInstance.ConnectionString + " " + secodInstance.ConnectionString);
+
+
+
+
+/// <summary>
+/// Singleton class that ensures only one instance of T can be created.
+/// Uses lazy initialization to create instance on first call to Instance().
+/// Allows passing initial instance on first call that will be returned.
+/// Subsequent calls ignore passed instance and return same cached instance.
+/// </summary>
+public static class Singleton<T> where T : new()
+{
+    private static T _instance;
+    private static object _lockObject = new();
+
+    /// <summary>
+    /// Returns the instance of T. If no instance exists, creates a new instance using the 'new()' constructor.
+    /// On subsequent calls, returns the same instance.
+    /// </summary>
+    /// <param name="instance">An instance of T to be used as the initial instance. This instance will be returned if no other instance exists.</param>
+    /// <returns>An instance of T.</returns>
+    public static T Instance(T instance)
+    {
+        lock (_lockObject)
+        {
+            if (_instance is null)
+            {
+                _instance = new();
+                _instance = instance;
+            }
+        }
+
+        return _instance;
+    }
+}
+
+
+public class Connection
+{
+    public string ConnectionString { get; set; }
+}
+
+
+
+
+
+
+
+/* Unit test
+
+public class SingletonTests
+{
+    [Fact]
+    public void Instance_WithNewInstance_ShouldReturnSameInstance()
+    {
+        var mock = new Mock<Connection>();
+        var instance1 = Singleton<Connection>.Instance(mock.Object);
+        var instance2 = Singleton<Connection>.Instance(mock.Object);
+
+        Assert.Same(instance1, instance2);
+    }
+
+    [Fact]
+    public void Instance_WithDifferentInstances_ShouldReturnFirstInstance()
+    {
+        var mock1 = new Mock<Connection>();
+        var mock2 = new Mock<Connection>();
+        var instance1 = Singleton<Connection>.Instance(mock1.Object);
+        var instance2 = Singleton<Connection>.Instance(mock2.Object);
+
+        Assert.Same(instance1, instance2);
+    }
+
+    class Connection
+    {
+
+    }
+}
+
+
+
+
+*/
