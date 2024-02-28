4917ed15e670be129404ff59e45093fd5810ef53
ozan <ozanakaydin@gmail.com>
2024-02-28 18:36:37 +0300

    updated

--- Creational/BuilderPattern/Program.cs
+++ Creational/BuilderPattern/Program.cs
@@ -1,22 +1,48 @@
 ﻿using System.Text;
 
+/*
+
+The purpose of this Builder Pattern code is to allow creating different configurations of Car objects without needing to specify all the details upfront. 
+It achieves this by using a CarBuilder class.
+
+The Car class represents a car object. It has properties like Id, Brand, Model, Color and MaxSpeed. The key thing is it has a private constructor, so a Car object 
+can only be created through the static Create method.
+
+The Create method takes an instance of the CarBuilder class. It creates a new Car instance and sets the Id property to a new random GUID. Then it returns this Car object.
+
+The CarBuilder class has a Car property to hold the Car it is building. It has methods like SetBrand, SetModel etc that allow setting the different properties on the Car. 
+Each method returns the CarBuilder instance so they can be chained.
+
+To use this, first an instance of CarBuilder is created. Then the Create method is called passing the builder to make a new Car. The builder methods are used to set the properties.
+
+This allows creating different Car configurations by calling Create with the same builder instance. The builder is reset each time to build a new Car. We don't need to specify 
+all details upfront.
+
+The key benefit is the client code can focus on building the object through a fluent interface. The complex construction logic is hidden inside the builder. This results in 
+cleaner client code and allows varying the internal construction logic later if needed.
+
+So in summary, the Builder pattern here encapsulates the construction process allowing creating different Car configurations through a simple builder interface. It separates
+the complex construction code from the clean client code.
+
+
+*/
 
 
 CarBuilder builder = new CarBuilder();
 
 Car mustang = Car.Create(builder);
- builder.SetBrand("Ford")
- .SetModel("Mustang")
- .SetMaxSpeed(320)
- .SetColor(Color.White)
- .WriteToConsole();
+builder.SetBrand("Ford")
+.SetModel("Mustang")
+.SetMaxSpeed(320)
+.SetColor(Color.White)
+.WriteToConsole();
 
- Car focus = Car.Create(builder);
- builder.SetBrand("Ford")
- .SetModel("Focus")
- .SetMaxSpeed(230)
- .SetColor(Color.Gray)
- .WriteToConsole();
+Car focus = Car.Create(builder);
+builder.SetBrand("Ford")
+.SetModel("Focus")
+.SetMaxSpeed(230)
+.SetColor(Color.Gray)
+.WriteToConsole();
 
 /*Output
 
@@ -37,13 +63,13 @@ Speed: 230
 
 class Car
 {
-    public string Id {get;private set;}
+    public string Id { get; private set; }
     public string Brand { get; set; }
     public string Model { get; set; }
     public string Color { get; set; }
     public int MaxSpeed { get; set; }
-    
-    private Car(){}
+
+    private Car() { }
 
     public static Car Create(CarBuilder carBuilder)
     {
@@ -57,7 +83,7 @@ class Car
 
 class CarBuilder
 {
-    public Car Car {get;set;}
+    public Car Car { get; set; }
     public CarBuilder SetBrand(string brand)
     {
         Car.Brand = brand;
@@ -87,22 +113,22 @@ class CarBuilder
         var result = new StringBuilder();
         result.AppendLine($"ID: {Car.Id}");
 
-        if(!string.IsNullOrEmpty(Car.Brand))
+        if (!string.IsNullOrEmpty(Car.Brand))
         {
             result.Append($"Brand: {Car.Brand}\n");
         }
 
-        if(!string.IsNullOrEmpty(Car.Model))
+        if (!string.IsNullOrEmpty(Car.Model))
         {
             result.Append($"Model: {Car.Model}\n");
         }
 
-        if(!string.IsNullOrEmpty(Car.Color))
+        if (!string.IsNullOrEmpty(Car.Color))
         {
             result.Append($"Color: {Car.Color}\n");
         }
 
-         if(Car.MaxSpeed > 0)
+        if (Car.MaxSpeed > 0)
         {
             result.Append($"Speed: {Car.MaxSpeed}\n");
         }
@@ -124,33 +150,6 @@ enum Color
 }
 
 
-
-/*
-
-The purpose of this Builder Pattern code is to allow creating different configurations of Car objects without needing to specify all the details upfront. 
-It achieves this by using a CarBuilder class.
-
-The Car class represents a car object. It has properties like Id, Brand, Model, Color and MaxSpeed. The key thing is it has a private constructor, so a Car object 
-can only be created through the static Create method.
-
-The Create method takes an instance of the CarBuilder class. It creates a new Car instance and sets the Id property to a new random GUID. Then it returns this Car object.
-
-The CarBuilder class has a Car property to hold the Car it is building. It has methods like SetBrand, SetModel etc that allow setting the different properties on the Car. 
-Each method returns the CarBuilder instance so they can be chained.
-
-To use this, first an instance of CarBuilder is created. Then the Create method is called passing the builder to make a new Car. The builder methods are used to set the properties.
-
-This allows creating different Car configurations by calling Create with the same builder instance. The builder is reset each time to build a new Car. We don't need to specify 
-all details upfront.
-
-The key benefit is the client code can focus on building the object through a fluent interface. The complex construction logic is hidden inside the builder. This results in 
-cleaner client code and allows varying the internal construction logic later if needed.
-
-So in summary, the Builder pattern here encapsulates the construction process allowing creating different Car configurations through a simple builder interface. It separates
-the complex construction code from the clean client code.
-
-
-*/
 
 
 /* Unit tests
